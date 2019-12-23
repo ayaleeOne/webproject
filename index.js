15def2a468c46d7f7c6c6a0e9f9f2b558a8130d3
Ayalee <180107236@stu.sdu.edu.kz>
2019-12-23 20:46:04 +0600

    express installed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+const server = express();
+
+server.use(bodyParser.json());
+server.use(express.static(path.join(__dirname, "static")));
+server.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url} at ${new Date().toTimeString()}`);
+    next();
+});
+
+server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "static", "homepage.html"));
+});
+
+server.listen(80, console.log("Server started"));
